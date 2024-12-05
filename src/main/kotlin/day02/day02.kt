f8427b80842ef77ffb0c72fe295b8a4c2ddf0c86
Vishnu Haridas <hvishnu999@gmail.com>
2024-12-05 14:09:08 +0400

    Day 02 Part 02

--- src/main/kotlin/day02/day02.kt
+++ src/main/kotlin/day02/day02.kt
@@ -2,6 +2,20 @@ package day02
 
 import util.getFileLines
 
+private fun differences(list: List<Int>) = list.zipWithNext { a, b -> b - a }
+
+private fun isDiffsSafe(differences: List<Int>) =
+    differences[0] > 0 && differences.all { d -> d in 1..3 }    // All positives within range 1..3
+            || differences[0] < 0 && differences.all { d -> d in -3..-1 } // or All negatives within range -3..-1
+
+private fun isLevelsSafe(levels: List<Int>) = isDiffsSafe(differences(levels))
+
+// Create a list with one item dropped in each iteration: [1,2,3,4] -> [2,3,4], [1,3,4], [1,2,4], [1,2,3]
+private fun <T> oneItemDropped(list: List<T>) = List(list.size) { index ->
+    list.filterIndexed { i, _ -> i != index }
+}
+
+
 fun main() {
 
     val linesOfNums = getFileLines("day02/input.txt")
@@ -13,16 +27,23 @@ fun main() {
         }
         .filterNot { it.isEmpty() }
 
+
     // Part 1: find the total number of "safe" levels.
     val safeList = linesOfNums
-        .map { it.zipWithNext { a, b -> b - a } } // Find the differences between each consecutive elements.
-        .filter {
-
-            it[0] > 0 && it.all { d -> d in 1..3 }    // All positives within range 1..3
-                    || it[0] < 0 && it.all { d -> d in -3..-1 } // or All negatives within range -3..-1
-
-        }
+        .filter { isLevelsSafe(it) }
 
     println("Total number of safe list: ${safeList.count()}")
+
+
+    // Part 2: Find the lists that can be safe by removing just one item.
+    val safeListWithTolerance = linesOfNums
+        .filter { !isLevelsSafe(it) } // Pick the unsafe items
+        .filter {
+            oneItemDropped(it) // Make a list with 1 item dropped in each iteration.
+                .any { dropped -> isLevelsSafe(dropped) } // Find if any of the items in that list is safe.
+            // There might be a better way than creating N lists and looping through each of them.
+        }
+
+    println("Total number of safe list with tolerance: ${safeList.count() + safeListWithTolerance.count()}")
 
 }
